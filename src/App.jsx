a49312861f67adb10cf7f9a22e9e469c2beb356f
Parth Deshpande <parthjun27@gmail.com>
2024-04-02 21:42:32 +0530

    creating routes

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <div className='bg-red-300'>
+    <h1 className='bg-red'>React router</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
