d9d3d1b277ee0261dd3ae6381fa3ce73adeb8b45
Parth Deshpande <parthjun27@gmail.com>
2024-04-02 22:15:41 +0530

    Create Routes

--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,21 @@ import Layout from './Layout.jsx'
 import { createBrowserRouter,createRoutesFromElements,Route, RouterProvider }from 'react-router-dom'
 import Home from './Components/home/Home.jsx'
 import About from './Components/about/About.jsx'
+import Contact from './Components/contact/Contact.jsx'
+import User from './Components/user/User.jsx'
+import Github from './Components/github/Github.jsx'
 
  const router=createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />}>
       <Route path="" element={<Home />}/>
       <Route path="about" element={<About />}/>
+      <Route path='contact' element={<Contact />}/>
+      <Route path='user/' element={<User />}>
+        <Route path=":userId" element={<User />}/>
+      </Route>
+      <Route path="github" element={<Github />}/>
+      <Route path="*" element={<div>Not Found</div>} />
     </Route>
   )
  )
